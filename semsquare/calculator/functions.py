1a9a4c54d9c8c4c747b3c2706e44fa35a2506b0a
captaincurry94 <leo.boes@yahoo.com>
2022-03-06 03:39:11 +0100

    pls in function

--- semsquare/calculator/functions.py
+++ semsquare/calculator/functions.py
@@ -86,40 +86,49 @@ input_json = json.loads(input)
 #just for testing use csv from github - later on read dataset from json
 dataset = pd.read_csv("https://raw.githubusercontent.com/GoogleCloudPlatform/plspm-python/trunk/tests/data/russa.csv", delimiter=",", index_col=0)
 
-### run pls path
-default_scale = Scale.NUM #Alternatives: Scale.NOM, Scale.ORD
-default_scheme = Scheme.CENTROID #Alternatives: Scheme.FACTORIAL Scheme.PATH
+def run_pls(input_json, dataset, default_scale="NUM"):
+    '''
+    Function that runs plspm based on json input of data and path relations
+    Takes: JSON with dataset and path relations
+    Returns: ???
+    '''
 
-scaling_dict = {"ORD": Scale.ORD,
-                "NUM": Scale.NUM,
-                "NOM": Scale.NOM,
-                "": default_scale}
+    scaling_dict = {"ORD": Scale.ORD,
+                    "NUM": Scale.NUM,
+                    "NOM": Scale.NOM,
+                    "": default_scale}
 
-mode_dict = {"reflective": Mode.A,
-             "formative": Mode.B}
+    mode_dict = {"reflective": Mode.A,
+                 "formative": Mode.B}
 
-scheme_dict = {"centroid": Scheme.CENTROID,
-               "factorial": Scheme.FACTORIAL,
-               "path": Scheme.PATH,
-               "": default_scheme}
+    scheme_dict = {"centroid": Scheme.CENTROID,
+                   "factorial": Scheme.FACTORIAL,
+                   "path": Scheme.PATH,}
 
-scheme = scheme_dict[input_json["scheme"]]
+    #set defaults
+    default_scale = scaling_dict[default_scale]
+    scheme = scheme_dict[input_json["scheme"]]
 
-#initialize structure
-structure = c.Structure()
+    #initialize structure
+    structure = c.Structure()
 
-#add paths to structure
-for latent_var in input_json["latent_variables"]:
-    if latent_var["pointed_lvs"]:
-        structure.add_path(latent_var["pointed_lvs"], [latent_var["name"]])
+    #add latent vars and their paths to structure
+    for latent_var in input_json["latent_variables"]:
+        if latent_var["pointed_lvs"]:
+            structure.add_path(latent_var["pointed_lvs"], [latent_var["name"]])
 
-config = c.Config(structure.path(), default_scale=default_scale)
-for latent_var in input_json["latent_variables"]:
-    if latent_var["pointed_mvs"]:
-        config.add_lv(latent_var["name"], mode_dict[latent_var["mode"]], *[c.MV(mv["name"], scaling_dict[mv["scaling"]]) if mv["scaling"] else c.MV(mv["name"]) for mv in latent_var["pointed_mvs"]])
+    #add manifest vars and their paths to latent vars
+    config = c.Config(structure.path(), default_scale=default_scale)
+    for latent_var in input_json["latent_variables"]:
+        if latent_var["pointed_mvs"]:
+            config.add_lv(latent_var["name"], mode_dict[latent_var["mode"]], *[c.MV(mv["name"], scaling_dict[mv["scaling"]]) if mv["scaling"] else c.MV(mv["name"]) for mv in latent_var["pointed_mvs"]])
 
-plspm_calc = Plspm(dataset, config, scheme, 100, 0.0000001)
+    #calculate plspm
+    plspm_calc = Plspm(dataset, config, scheme, 100, 0.0000001)
 
-print(plspm_calc.inner_summary())
-print(plspm_calc.effects())
+    #output data
+    print(plspm_calc.inner_summary())
+    print(plspm_calc.effects())
+
+run_pls(input_json=input_json, dataset=dataset)
 
